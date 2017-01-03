6d79ac5f298f2dea8a618ddd9a30e62a28652b61
furgat <laguz.switch@gmail.com>
2017-01-02 17:07:13 -0800

    now supports clearing all data. deferred user confirmation implemented with factory qInput

--- js/errand-boy.js
+++ js/errand-boy.js
@@ -1,11 +1,30 @@
 var application = angular.module('furgatErrandBoy', ['ngCookies']);
-var appversion = '1.0.Atomic';
+var appversion = '1.1 Bottlecap';
 
-application.controller('errandBoy', function($scope, $cookies) {
+//=
+// qInput :
+// deferred input factory used to prompt the user asynchronously
+//=
+application.factory('qInput', ['$window', '$q', function($window, $q){
+    return {
+        confirm: function(msg) {
+            var defer = $q.defer();
+            // TODO: replace native confirm with a custom solution
+            if ( $window.confirm(msg) ) {
+                defer.resolve(true);
+            } else {
+                defer.reject(false);
+            }
+            return defer.promise;
+        }
+    };
+}]);
+
+application.controller('errandBoy', ['$scope', '$cookies', 'qInput', function($scope, $cookies, qInput) {
     
-    //==============================================================================================
+    //=
     // initialization of data :
-    //==============================================================================================
+    //=
     // declare our errands array
     // errands are formatted as follows
     // [
@@ -33,6 +52,8 @@ application.controller('errandBoy', function($scope, $cookies) {
     // set form to hidden
     $scope.adding = false;
     
+    // cookies permissions
+    var cookiesPermission = $cookies.get('furgatErrandBoyPermission');
     
     // attempt to load previous errands from cookies
     var lastErrands = $cookies.getObject('furgatErrandBoyErrands');
@@ -41,30 +62,41 @@ application.controller('errandBoy', function($scope, $cookies) {
     if ( lastErrands !== undefined )
         $scope.errands = lastErrands;
     
-    //==============================================================================================
+    //=
     // saveErrands :
     // helper function, saves out the array of errands into a cookie using $cookies
-    //==============================================================================================
+    //=
     $scope.saveErrands = function () {
-        // wrap in a try-catch block?
-        // save to cookies
-        $cookies.putObject('furgatErrandBoyErrands', $scope.errands);    
-        // display success message
+        // prompt user for permission if permission is undefined
+        if ( cookiesPermission == undefined )
+        {
+            qInput.confirm('Allow Errand Boy to store Data in your Cookies?').then(
+                function(response) { // success means 'okay' was pressed
+                    cookiesPermission = true;
+                },
+                function() { // failure means 'cancel' was pressed
+                    cookiesPermission = false;
+                }
+            );
+        }
+        
+        // if allowed to, save to cookies
+        if (cookiesPermission) $cookies.putObject('furgatErrandBoyErrands', $scope.errands);    
     };
     
-    //==============================================================================================
+    //=
     // toggleAdd :
     // called from the toggle frontend button, enables/disables the errand form
-    //==============================================================================================
+    //=
     $scope.toggleAdd = function() {
         var tempBool = $scope.adding;
         $scope.adding = !tempBool;
     }
     
-    //==============================================================================================
+    //=
     // addErrand : 
     // called from the form button, takes filled in data to add a new errand
-    //==============================================================================================
+    //=
     $scope.addErrand = function(obj) {
         // temporary variable to store user input
         var temps = [$scope.errandForm.name, $scope.errandForm.desc];
@@ -72,7 +104,6 @@ application.controller('errandBoy', function($scope, $cookies) {
         if ( temps[0] == undefined || temps[0] == '' ) { // reject blank name input, blank description is fine
             $scope.report('You must at least enter a Name for your errand,', 'pink');
         } else {
-            // should report on success state as well
             $scope.errands.push({
                 'name' : $scope.errandForm.name,
                 'desc' : $scope.errandForm.desc,
@@ -90,10 +121,10 @@ application.controller('errandBoy', function($scope, $cookies) {
         }
     };
     
-    //==============================================================================================
+    //=
     // editErrands :
     // called from ng-dblclick, allows errand names, descs to be edited with contenteditable
-    //==============================================================================================
+    //=
     $scope.editErrand = function(index, kind) {
         var editValue = event.target.contentEditable; // we don't need to constantly refer to event
 
@@ -125,65 +156,83 @@ application.controller('errandBoy', function($scope, $cookies) {
         event.target.contentEditable = editValue; // alter content editable on the element
     };
     
-    //==============================================================================================
+    //=
     // editEnter :
     // processes 'enter' keypress when editing, calls editErrand to close and save
     // the reason for re-using editErrand is that it can still be called on ng-dbclick even
     // when open, so we want both dblclick and enter presses to behave properly
-    //==============================================================================================
+    //=
     $scope.editEnter = function(index, kind) {
         if (event.which == 13) {
             $scope.editErrand(index, kind)
         }
     };
     
-    //==============================================================================================
+    //=
     // statusUpdate :
     // called from checkbox click, forces a save (the checkbox itself updates e.status)
-    //==============================================================================================
+    //=
     $scope.statusUpdate = function(index) {
         $scope.saveErrands();
     }
     
-    //==============================================================================================
+    //=
     // delErrand :
     // called from the individual trash buckets, deletes the task that trash bucket is on
-    //==============================================================================================
+    //=
     $scope.delErrand = function(index) {
-        $scope.errands.splice(index, 1); // remove at [index] length 1
-        $scope.saveErrands(); // save after removing a task
+        if (index != undefined) { // prevent bad input
+            $scope.errands.splice(index, 1); // remove at [index] length 1
+            $scope.saveErrands(); // save after removing a task
+        }
     };
     
-    //==============================================================================================
+    //=
     // report :
+    // > msg - string, message to display in the report
+    // > color - string, css class [green/pink]
     // internally used to add a new report to the ticker
-    //==============================================================================================
+    //=
     $scope.report = function(msg, color) {
+        // default msg to empty string and color to green if either is invalid
+        if ( msg == undefined ) msg = '';
+        if ( color == undefined || (color != 'green' && color != 'pink')) color = 'green';
+        
+        // add the report to the array
         $scope.reports.push({
             'message' : msg,
             'color' : color
         });
     }
     
-    //==============================================================================================
+    //=
     // delReport :
     // called from the close button in the ui to remove a report from the ticker
-    //==============================================================================================
+    //=
     $scope.delReport = function(index) {
         $scope.reports.splice(index, 1);
     }
     
-    //==============================================================================================
+    //=
     // clearAllData :
-    // called from the clear button, removes all tasks and resets the cookie
-    //==============================================================================================
+    // called from the clear confirm button, removes all tasks and resets the cookies
+    //=
     $scope.clearAllData = function () {
-        // delete everything from the errands list
-        $scope.errands = [];
-        // completely empty the cookie by saving the empty list
-        $scope.saveTasks();
+        // ask for permission first, to protect against accidental clicks
+        qInput.confirm('Really Delete ALL Errands, and Remove any/all Cookies?').then(
+            function(response) { // okay was pressed
+                // delete everything from the errands list
+                $scope.errands = [];
+                // remove the cookies completely
+                $cookies.remove('furgatErrandBoyErrands');
+                $cookies.remove('furgatErrandBoyPermission');
+            },
+            function() {
+                // do nothing, cancel was pressed
+            }
+        );
     };
     
     // welcome the user
     $scope.report('Welcome to Errand-Boy (ver. ' + appversion + ')', 'green');
-});
+}]);
